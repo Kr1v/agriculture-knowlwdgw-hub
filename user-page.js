82729768620d4f4412b247d73c201540c1be5681
Kritika Mehta <162875055+Kr1v@users.noreply.github.com>
2024-09-14 10:05:21 +0530

    Update user-page.js

--- user-page.js
+++ user-page.js
@@ -1,15 +1,13 @@
-//function toggleVisibility(sectionId) {
+
+
+
+      
+function toggleVisibility(sectionId) {
     var content = document.getElementById(sectionId);
     if (content.style.display === "none" || content.classList.contains('hidden')) {
         content.style.display = "block";
         content.classList.remove('hidden');
     } else {
-        content.style.display = "none";
+        content.style.display === "none";
         content.classList.add('hidden');
-    } //
-
-
-    // scripts.js
-function toggleContent(element) {
-    element.classList.toggle('active');
-}
+    } }
